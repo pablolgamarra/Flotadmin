bfd1c24e4dda0faf16573f4b4425a38cc4d514a7
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-11-11 15:49:22 -0300

    First Commit

---
+++ src/webparts/flotadminListaVehiculos/components/IFlotadminListaVehiculosProps.ts
@@ -0,0 +1,7 @@
+export interface IFlotadminListaVehiculosProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
