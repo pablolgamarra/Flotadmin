4cd1c9ad4ddc5dc3c8f92ab28ded6d9f3f797d75
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-11-12 10:36:21 -0300

    Removed template files

--- src/webparts/flotadminListaVehiculos/components/IFlotadminListaVehiculosProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IFlotadminListaVehiculosProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
