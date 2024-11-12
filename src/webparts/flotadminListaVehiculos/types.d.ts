f4c6bf4903e16280c6f6c2c2c6d2339ef27f4b27
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-11-12 10:36:07 -0300

    Add Types

---
+++ src/webparts/flotadminListaVehiculos/types.d.ts
@@ -0,0 +1,38 @@
+enum Currency {
+	Dolar = 'Dolar',
+	Guaranies = 'Guaranies',
+}
+
+export interface FleetCard {
+	Id: number;
+	CardNumber: string;
+	AsignedValue: string;
+}
+
+export interface Vehicle {
+	Id: number;
+	Plate: string;
+	Brand: string;
+	Model: string;
+	ModelYear: string;
+	BuyDate: Date;
+	Cost: number;
+	CostCurrency: Currency;
+	User: string;
+	FleetCard: FleetCard;
+}
+
+export interface InterventionType {
+	Id: number;
+	Description: string;
+}
+
+export interface Intervention {
+	Id: number;
+	Kilometers: string;
+	Vehicle: Vehicle;
+	Date: Date;
+	IntervationType: IntervationType;
+	Cost: number;
+	CostCurrency: Currency;
+}
