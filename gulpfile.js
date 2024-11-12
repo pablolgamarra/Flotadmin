97552a436540ef057c1478ed3169a6203893d80c
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-11-12 17:24:47 -0300

    Add tailwind css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 const path = require('path');
 const build = require('@microsoft/sp-build-web');
 
+const postcss = require('gulp-postcss');
+const atimport = require('postcss-import');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwind = require('tailwindcss');
+
 build.addSuppression(
 	`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`,
 );
@@ -30,5 +35,30 @@ build.configureWebpack.mergeConfig({
 		return generatedConfiguration;
 	},
 });
+
+const tailwindcss = build.subTask(
+	'tailwindcss',
+	function (gulp, buildOptions, done) {
+		gulp.src('assets/tailwind.css')
+			.pipe(
+				postcss([
+					atimport(),
+					tailwind('./tailwind.config.js'),
+					...(buildOptions.args.ship
+						? [
+								purgecss({
+									content: ['src/**/*.tsx'],
+									defaultExtractor: (content) =>
+										content.match(/[\w-/:]+(?<!:)/g) || [],
+								}),
+						  ]
+						: []),
+				]),
+			)
+			.pipe(gulp.dest('assets/dist'));
+		done();
+	},
+);
+build.rig.addPreBuildTask(tailwindcss);
 
 build.initialize(require('gulp'));
