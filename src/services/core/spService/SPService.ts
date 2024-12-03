63ff46ea6cfaf0e7c152c44a9f48dafa9bfd499d
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-12-03 10:11:37 -0300

    Fix bug on SPService

--- src/services/core/spService/SPService.ts
+++ src/services/core/spService/SPService.ts
@@ -1,8 +1,10 @@
 import { ServiceKey, ServiceScope } from '@microsoft/sp-core-library';
 import { ISPService } from './ISPService';
 import { spfi, SPFI, SPFx } from '@pnp/sp';
+import '@pnp/sp/webs';
+import '@pnp/sp/lists';
+import '@pnp/sp/items';
 import { PageContext } from '@microsoft/sp-page-context';
-import { WebPartContext } from '@microsoft/sp-webpart-base';
 
 export class SPService implements ISPService {
 	public static readonly servicekey: ServiceKey<ISPService> =
@@ -10,13 +12,17 @@ export class SPService implements ISPService {
 
 	private _sp!: SPFI;
 
-	constructor(serviceScope:ServiceScope) {
-        serviceScope.whenFinished(()=>{
-            this._sp = serviceScope.consume(PageContext.servicekey)
-        })
+	constructor(serviceScope: ServiceScope) {
+		serviceScope.whenFinished(() => {
+			const pageContext = serviceScope.consume(PageContext.serviceKey);
+			this._sp = spfi().using(SPFx({ pageContext }));
+		});
 	}
 
 	public async getListItems(listName: string): Promise<any[]> {
-		const queryResult  = await this._sp.web.lists.
+		const queryResult = await this._sp.web.lists
+			.getByTitle(listName)
+			.items();
+		return queryResult;
 	}
 }
