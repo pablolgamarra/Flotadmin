47df196129eafa9afb2916f70447d523db16fac2
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2024-12-18 15:55:44 -0300

    Add input types on InputField

--- src/controls/InputField.tsx
+++ src/controls/InputField.tsx
@@ -3,13 +3,26 @@ import * as React from 'react';
 import { Field, Input, InputOnChangeData } from '@fluentui/react-components';
 
 export interface InputFieldProps {
-	id: string;
-	label: string;
-	name: string;
-	placeholder: string;
-	type?: string;
-	value: string | number | undefined;
-	onChange: (ev: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => void;
+	id?: string;
+	label?: string;
+	name?: string;
+	placeholder?: string;
+	type?:
+		| 'number'
+		| 'time'
+		| 'text'
+		| 'search'
+		| 'email'
+		| 'password'
+		| 'tel'
+		| 'url'
+		| 'date'
+		| 'datetime-local'
+		| 'month'
+		| 'week'
+		| undefined;
+	value?: string | number | undefined;
+	onChange?: (ev: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => void;
 	required?: boolean;
 }
 
@@ -31,6 +44,7 @@ export const InputField: React.FC<InputFieldProps> = ({
 		>
 			<Input
 				name={name}
+				type={type}
 				placeholder={placeholder}
 				value={value?.toString()}
 				onChange={onChange}
