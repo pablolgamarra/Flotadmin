3ef4c9f8ab57164517c808ab95dea6838a558a1c
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2025-01-02 16:33:43 -0300

    Add interventionList visualization handlers on VehicleDataVisualizer, adding message when list is empty

--- src/controls/vehicles/dialog/content/visualize/VehicleDataVisualizer.tsx
+++ src/controls/vehicles/dialog/content/visualize/VehicleDataVisualizer.tsx
@@ -5,7 +5,7 @@ import { useDataContext } from '@/hooks/useDataContext';
 import { useInterventionList } from '@/hooks/useInterventionList';
 import { FleetCard } from '@/models/FleetCard';
 import { Vehicle } from '@/models/Vehicle';
-import { Divider, Spinner, Title3 } from '@fluentui/react-components';
+import { Body1, Divider, Spinner, Title3 } from '@fluentui/react-components';
 import { VehicleDataFields } from './VehicleDataFields';
 import { VehicleDataInteractions } from './VehicleDataInteractions';
 
@@ -31,26 +31,31 @@ export const VehicleDataVisualizer: React.FC<VehicleDataFormProps> = (props) => 
 	const { interventionsService } = useDataContext();
 
 	const { isLoading, interventionList } = useInterventionList(interventionsService, vehicle?.Id);
+    const hasInterventions = interventionList.length > 0;
+    const noInterventions = !isLoading && !hasInterventions;
 
-	return (
-		<>
-			<VehicleDataFields vehicle={vehicle} />
-			<Divider
-				className='tw-mt-5'
-				title='Intervenciones'
-				alignContent='start'
-			/>
-			{isLoading ? (
-				<Spinner
-					label='Leyendo Interacciones del Vehiculo'
-					size='tiny'
-				/>
-			) : (
-				<>
-					<Title3 className='tw-flex tw-justify-center mt-5'>Intervenciones del Vehiculo</Title3>
-					<VehicleDataInteractions interactions={interventionList} />
-				</>
-			)}
-		</>
+    return (
+        <>
+            <VehicleDataFields vehicle={vehicle} />
+
+            <Divider className="tw-mt-5" title="Intervenciones" alignContent="start" />
+
+            {isLoading && (
+                <Spinner label="Leyendo Interacciones del Vehículo" size="tiny" />
+            )}
+
+            {hasInterventions && (
+                <>
+                    <Title3 className="tw-flex tw-justify-center mt-5">Intervenciones del Vehículo</Title3>
+                    <VehicleDataInteractions interactions={interventionList} />
+                </>
+            )}
+
+            {noInterventions && (
+                <Body1 className="tw-flex tw-justify-center mt-5 tw-text-gray-400">
+                    Este vehículo aún no tiene intervenciones realizadas
+                </Body1>
+            )}
+        </>
 	);
 };
